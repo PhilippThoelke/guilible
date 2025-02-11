e0bbdfe5c517bc35057165d257ec471452e78177
PhilippThoelke <philipp.thoelke@posteo.de>
2025-02-11 16:52:18 +0000

    Basic winit and wgpu setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,147 @@
+use std::sync::Arc;
+
+use pollster::FutureExt;
+use wgpu;
+use winit::application::ApplicationHandler;
+use winit::event::WindowEvent;
+use winit::event_loop::{ControlFlow, EventLoop};
+use winit::window::Window;
+
+#[derive(Default)]
+struct AppState<'a> {
+    render_state: Option<RenderState<'a>>,
+    window: Option<Arc<Window>>,
+}
+
+impl<'a> ApplicationHandler for AppState<'a> {
+    fn resumed(&mut self, event_loop: &winit::event_loop::ActiveEventLoop) {
+        println!("application resumed");
+        let win_arc = Arc::new(
+            event_loop
+                .create_window(Window::default_attributes())
+                .unwrap(),
+        );
+        self.window = Some(win_arc.clone());
+        self.render_state = Some(RenderState::new(win_arc.clone()));
+    }
+
+    fn window_event(
+        &mut self,
+        event_loop: &winit::event_loop::ActiveEventLoop,
+        window_id: winit::window::WindowId,
+        event: winit::event::WindowEvent,
+    ) {
+        match event {
+            WindowEvent::CloseRequested => {
+                event_loop.exit();
+            }
+            WindowEvent::Resized(size) => {
+                self.render_state.as_mut().unwrap().resize(size);
+            }
+            WindowEvent::RedrawRequested => {
+                self.render_state.as_mut().unwrap().render().unwrap();
+                self.window.as_ref().unwrap().request_redraw();
+            }
+            _ => {}
+        }
+    }
+}
+
+struct RenderState<'a> {
+    surface: wgpu::Surface<'a>,
+    config: wgpu::SurfaceConfiguration,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+
+    window: Arc<Window>,
+}
+
+impl<'a> RenderState<'a> {
+    fn new(window: Arc<Window>) -> Self {
+        println!("creating render state");
+
+        let window_size = window.inner_size();
+
+        let instance = wgpu::Instance::new(&wgpu::InstanceDescriptor {
+            backends: wgpu::Backends::PRIMARY,
+            ..Default::default()
+        });
+
+        let surface = instance.create_surface(window.clone()).unwrap();
+        let adapter = instance
+            .request_adapter(&wgpu::RequestAdapterOptions {
+                power_preference: wgpu::PowerPreference::HighPerformance,
+                compatible_surface: Some(&surface),
+                force_fallback_adapter: false,
+            })
+            .block_on()
+            .unwrap();
+
+        let (device, queue) = adapter
+            .request_device(
+                &wgpu::DeviceDescriptor {
+                    required_features: wgpu::Features::empty(),
+                    required_limits: wgpu::Limits::default(),
+                    label: None,
+                    memory_hints: wgpu::MemoryHints::default(),
+                },
+                None,
+            )
+            .block_on()
+            .unwrap();
+
+        let surface_capabilities = surface.get_capabilities(&adapter);
+        let surface_format = surface_capabilities
+            .formats
+            .iter()
+            .find(|format| format.is_srgb())
+            .copied()
+            .unwrap_or(surface_capabilities.formats[0]);
+        let config = wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            format: surface_format,
+            width: window_size.width,
+            height: window_size.height,
+            present_mode: surface_capabilities.present_modes[0],
+            alpha_mode: surface_capabilities.alpha_modes[0],
+            view_formats: vec![],
+            desired_maximum_frame_latency: 2,
+        };
+        surface.configure(&device, &config);
+
+        Self {
+            surface,
+            config,
+            device,
+            queue,
+            window,
+        }
+    }
+
+    pub fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
+        println!("rendering");
+
+        let output = self.surface.get_current_texture().unwrap();
+        output.present();
+
+        Ok(())
+    }
+
+    fn resize(&mut self, size: winit::dpi::PhysicalSize<u32>) {
+        println!("resizing to {:?}", size);
+        if size.width > 0 && size.height > 0 {
+            self.config.width = size.width;
+            self.config.height = size.height;
+            self.surface.configure(&self.device, &self.config);
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let event_loop = EventLoop::new().unwrap();
+    // TODO: we might need ControlFlow::Poll to handle updating the UI based on incoming data?
+    event_loop.set_control_flow(ControlFlow::Wait);
+
+    let mut app = AppState::default();
+    let _ = event_loop.run_app(&mut app);
 }
